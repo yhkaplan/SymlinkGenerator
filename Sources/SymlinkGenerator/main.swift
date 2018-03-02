7706d926448a1090341776c4836a71301d7324dd
yhkaplan <yhkaplan@gmail.com>
2018-03-02 23:44:43 +0900

    Experimenting w/ options

--- Sources/SymlinkGenerator/main.swift
+++ Sources/SymlinkGenerator/main.swift
@@ -3,15 +3,14 @@ import Commander
 
 let symgen = SymlinkGeneratorCore()
 
-print("Starting, please specify path to symlink file")
-
-command (
-    Argument<String>("path", description: "Path to symlink file")
-) { path in
+let main = command { (path: String) in
     do {
+        print("Generating symlinks for \(path)")
         let targets = try symgen.readSymlinkFile(at: path)
         targets?.forEach { print("\($0)") }
     } catch let error {
         print("Error: \(error)")
     }
-}.run()
+}
+    
+main.run()
