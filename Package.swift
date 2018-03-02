b45cdf1afebfc720f53174ae967cdf363ded72c0
yhkaplan <yhkaplan@gmail.com>
2018-03-02 16:12:08 +0900

    Fixed Package.swift file

--- Package.swift
+++ Package.swift
@@ -4,25 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "SymlinkGenerator": [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    name: "SymlinkGenerator",
+    dependencies: [
+		.package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
+	    .package(url: "https://github.com/Swiftline/Swiftline.git", from: "0.5.0")
     ],
     targets: [
         .target(
             name: "SymlinkGenerator",
-            dependencies: [
-		"SymlinkGeneratorCore",
-		// This is a CLI Swift utility for input/output
-	    	.package(url: "https://github.com/Swiftline/Swiftline.git", majorVersion: 0, minor: 3), 
-	    ]
-	),
-	.target(
-	    name: "SymlinkGeneratorCore",
-	    dependencies: [
-		// This is used for executing shell commands
-		.package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0")
-	    ]
-	)
+            dependencies: ["SymlinkGeneratorCore"]
+	    ),
+	    .target(
+	        name: "SymlinkGeneratorCore",
+	        dependencies: ["ShellOut", "Swiftline"]
+	    )
     ]
 )
---
+++ Sources/SymlinkGeneratorCore/SymlinkGenerator.swift
@@ -0,0 +1,13 @@
+import ShellOut
+
+class SymlinkGenTools {
+    private let symlinkFilePath: String
+    
+    do {
+        //toImplement
+        try shellOut(to: .readFile(at: symlinkFilePath) 
+    } catch let error {
+        print("Error: \(error)")
+        exit(0)
+    }
+}
