acec1da525062194c6e87f65d22de84bf596dbec
Joshua Kaplan <yhkaplan@gmail.com>
2018-03-03 13:51:07 +0900

    Fixed main.swift

--- Sources/SymlinkGenerator/main.swift
+++ Sources/SymlinkGenerator/main.swift
@@ -6,10 +6,12 @@ let symgen = SymlinkGeneratorCore()
 let main = command { (path: String) in
     do {
         print("Generating symlinks for \(path)")
-        let results = try symgen.readFile(at: "path", operation: symgen.extractTargetLinks)
+        try symgen.readFile(at: path, operation: symgen.extractTargetLinks)
+            .forEach { try symgen.generateSymlinks(targetLink: $0) }
+        
     } catch let error {
-        print("Error: \(error)")
+        print("Error: \(error.localizedDescription)")
     }
 }
-    
+
 main.run()
